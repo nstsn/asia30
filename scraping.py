370be02461b1f9589716e1edd0aab1cc79a42413
nstsn <nestaichi@gmail.com>
2020-06-13 20:54:46 +0900

    ChormeIndexの修正

--- scraping.py
+++ scraping.py
@@ -1,8 +1,8 @@
-from selenium import webdriver
+import chromedriver_binary
+from selenium import webdriver 
 import time
 
 driver = webdriver.Chrome()
 driver.get('https://www.forbes.com/under30/list/2019/asia/#3075a58b7572')
 time.sleep(3)
-click_element = driver.find_element_by_class_name("icon icon-search")
-
+click_element = driver.find_element_by_css_selector(".icon.icon-search")
